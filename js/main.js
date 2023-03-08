290a4b800e30481b72a3fcedef4bee0a23956af7
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-08 15:17:01 +0100

    Array di oggetti creato

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+// array di oggetti (ogni oggetto è un membro del team)
+const ourTeam = [
+    {
+        name : "Wayne Barnett",
+        rule : "Founder & CEO",
+        photo : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name : "Angela Caroll",
+        rule : "Chief Editor",
+        photo : "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name : "Walter Gordon",
+        rule : "Office Manager",
+        photo : "walter-gordon-office-manager.jpg"
+    },
+    {
+        name : "Angela Lopez",
+        rule : "Social Media Manager",
+        photo : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name : "Scott Estrada",
+        rule : "Developer",
+        photo : "scott-estrada-developer.jpg"
+    },
+    {
+        name : "Barbara Ramos",
+        rule : "Graphic Designer",
+        photo : "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+console.log(ourTeam);
